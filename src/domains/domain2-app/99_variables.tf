78f9438849dd8be3d6e2e9c81d20c1481650dbc9
pasqualedevita <>
2023-02-06 20:32:29 +0100

    fix

--- src/domains/domain2-app/99_variables.tf
+++ src/domains/domain2-app/99_variables.tf
@@ -12,7 +12,7 @@ variable "domain" {
   type    = string
   validation {
     condition = (
-      length(var.prefix) < 6
+      length(var.domain) < 6
     )
     error_message = "Max length is 6 chars."
   }
