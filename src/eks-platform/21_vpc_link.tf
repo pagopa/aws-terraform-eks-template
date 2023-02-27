e21f2937086fb9264ae8f8486599c70e487e2f1f
Matteo Cellucci <matteo.cellucci@keypartner.com>
2023-02-27 13:33:46 +0100

    feat: add ACK ApiGatewayV2 controller

--- src/eks-platform/21_vpc_link.tf
+++
@@ -1,6 +0,0 @@
-# resource "aws_api_gateway_vpc_link" "this" {
-#   name        = local.project
-#   description = "${local.project} link to cluster NLB"
-#   target_arns = [module.nlb.lb_arn]
-# }
-
