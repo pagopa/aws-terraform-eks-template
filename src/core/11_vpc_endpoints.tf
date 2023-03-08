ce28b29234c10fe0149d1834042f9442f9202a9d
Matteo Cellucci <matteo.cellucci@keypartner.com>
2023-03-08 11:55:54 +0100

    feat: add DynamoDB VPC endpoint

---
+++ src/core/11_vpc_endpoints.tf
@@ -0,0 +1,29 @@
+data "aws_vpc_endpoint_service" "dynamodb" {
+  service = "dynamodb"
+}
+
+resource "aws_vpc_endpoint" "dynamodb" {
+  vpc_id       = module.vpc.vpc_id
+  service_name = data.aws_vpc_endpoint_service.dynamodb.service_name
+}
+
+resource "aws_vpc_endpoint_route_table_association" "intra_dynamodb" {
+  for_each = toset(module.vpc.intra_route_table_ids)
+
+  vpc_endpoint_id = aws_vpc_endpoint.dynamodb.id
+  route_table_id  = each.value
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_dynamodb" {
+  for_each = toset(module.vpc.private_route_table_ids)
+
+  vpc_endpoint_id = aws_vpc_endpoint.dynamodb.id
+  route_table_id  = each.value
+}
+
+resource "aws_vpc_endpoint_route_table_association" "public_dynamodb" {
+  for_each = toset(module.vpc.public_route_table_ids)
+
+  vpc_endpoint_id = aws_vpc_endpoint.dynamodb.id
+  route_table_id  = each.value
+}
